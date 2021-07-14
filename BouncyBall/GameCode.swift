cce58140413671db9a480d2ec6b5b67ed7c2f620
Azhibek Abdrakhmanov <azhik459@icloud.com>
2021-07-14 22:44:15 +0600

    Started creating a game during Course

--- BouncyBall/GameCode.swift
+++ BouncyBall/GameCode.swift
@@ -10,6 +10,26 @@ for example if statements and for loops, at the top level; they have to be writt
 of a function.
 */
 
+let circle = OvalShape(width: 150, height: 150)
+let barrierHeight = 25.0
+let barrierWidth = 300.0
+
+let barrierPoints = [
+    Point(x: 0, y: 0),
+    Point(x: 0, y: barrierHeight),
+    Point(x: barrierWidth, y: barrierHeight),
+    Point(x: barrierWidth, y: 0)
+]
+
+let barrier = PolygonShape(points: barrierPoints)
+
 func setup() {
+    circle.position = Point(x: 250, y: 400)
+    barrier.position = Point(x: 200, y: 150)
+
+    circle.hasPhysics = true
+    barrier.hasPhysics = true
     
+    scene.add(circle)
+    scene.add(barrier)
 }
